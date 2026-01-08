a45d6a205dd3901ada93f185962371f74e8a3e99
kazi tareq <kazitareq321@gmail.com>
2026-01-08 13:51:09 +0600

    protect route

--- src/app/context/authprovider.tsx
+++ src/app/context/authprovider.tsx
@@ -2,38 +2,53 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { getMe } from "../actions/getme";
 import axios from "@/lib/axioss";
+import { useRouter } from "next/router";
 
 interface AuthContextType {
   user: any;
   setUser: (user: any) => void;
   logout: () => Promise<void>;
+  loading: any
 }
 
 const AuthContext = createContext<AuthContextType | null>(null);
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = useState<any>(null);
+  const router = useRouter();
+  const [loading, setloading] = useState(true);
 
   useEffect(() => {
     const fetchUser = async () => {
-      const data = await getMe();
-      setUser(data);
+      try {
+        const res = await axios.get("/auth/me");
+        setUser(res.data.user);
+      } catch {
+        setUser(null);
+      } finally {
+        setloading(false);
+      }
     };
     fetchUser();
   }, []);
 
-//   logout 
+  //   logout
 
- const logout = async () => {
+  const logout = async () => {
     try {
       await axios.post("/api/v1/auth/logout", {}, { withCredentials: true });
-      setUser(null); // remove user from state
+      setUser(null); 
+      router.push("/login");
     } catch (error) {
       console.log("Logout Error:", error);
     }
   };
 
-  return <AuthContext.Provider value={{ user, setUser,logout }}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={{ user, setUser, logout , loading}}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const useAuth = () => {
